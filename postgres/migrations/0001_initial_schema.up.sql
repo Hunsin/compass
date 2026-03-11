fa95c26888faac57d9a9398ddc7645f2bfa20461
Hsiaochieh Tsai <asky.hunsin@gmail.com>
2026-03-11 11:39:58 +0800

    Distinguish SQL data type and key words by letter case

--- postgres/migrations/0001_initial_schema.up.sql
+++ postgres/migrations/0001_initial_schema.up.sql
@@ -1,26 +1,26 @@
 CREATE TABLE exchanges (
-    abbr TEXT PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    timezone TEXT NOT NULL
+    abbr     text PRIMARY KEY,
+    name     text NOT NULL UNIQUE,
+    timezone text NOT NULL
 );
 
 CREATE TABLE securities (
-    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    exchange TEXT NOT NULL,
-    symbol   TEXT NOT NULL,
-    name     TEXT NOT NULL,
+    id       uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    exchange text NOT NULL,
+    symbol   text NOT NULL,
+    name     text NOT NULL,
     UNIQUE (exchange, symbol),
     FOREIGN KEY (exchange) REFERENCES exchanges(abbr) ON DELETE CASCADE
 );
 
 CREATE TABLE ohlcv_per_min (
-    sec_id    UUID NOT NULL,
-    ts   TIMESTAMP NOT NULL,
-    open   NUMERIC NOT NULL,
-    high   NUMERIC NOT NULL,
-    low    NUMERIC NOT NULL,
-    close  NUMERIC NOT NULL,
-    volume  BIGINT NOT NULL,
+    sec_id    uuid NOT NULL,
+    ts   timestamp NOT NULL,
+    open   numeric NOT NULL,
+    high   numeric NOT NULL,
+    low    numeric NOT NULL,
+    close  numeric NOT NULL,
+    volume  bigint NOT NULL,
     UNIQUE (sec_id, ts),
     FOREIGN KEY (sec_id) REFERENCES securities(id) ON DELETE CASCADE
 ) PARTITION BY RANGE (ts);
@@ -31,13 +31,13 @@ CREATE INDEX idx_ohlcv_per_min_sec_id_ts ON ohlcv_per_min (sec_id, ts);
 -- per-minute table, so we use ON CONFLICT DO UPDATE to handle both insertions and updates.
 -- The timestamps are always aligned to 30-minute boundaries.
 CREATE TABLE ohlcv_per_30min (
-    sec_id    UUID NOT NULL,
-    ts   TIMESTAMP NOT NULL,
-    open   NUMERIC NOT NULL,
-    high   NUMERIC NOT NULL,
-    low    NUMERIC NOT NULL,
-    close  NUMERIC NOT NULL,
-    volume  BIGINT NOT NULL,
+    sec_id    uuid NOT NULL,
+    ts   timestamp NOT NULL,
+    open   numeric NOT NULL,
+    high   numeric NOT NULL,
+    low    numeric NOT NULL,
+    close  numeric NOT NULL,
+    volume  bigint NOT NULL,
     UNIQUE (sec_id, ts),
     FOREIGN KEY (sec_id) REFERENCES securities(id) ON DELETE CASCADE
 ) PARTITION BY RANGE (ts);
@@ -45,13 +45,13 @@ CREATE TABLE ohlcv_per_30min (
 CREATE INDEX idx_ohlcv_per_30min_sec_id_ts ON ohlcv_per_30min (sec_id, ts);
 
 CREATE TABLE ohlcv_per_day (
-    sec_id    UUID NOT NULL,
-    date      DATE NOT NULL,
-    open   NUMERIC NOT NULL,
-    high   NUMERIC NOT NULL,
-    low    NUMERIC NOT NULL,
-    close  NUMERIC NOT NULL,
-    volume  BIGINT NOT NULL,
+    sec_id    uuid NOT NULL,
+    date      date NOT NULL,
+    open   numeric NOT NULL,
+    high   numeric NOT NULL,
+    low    numeric NOT NULL,
+    close  numeric NOT NULL,
+    volume  bigint NOT NULL,
     UNIQUE (sec_id, date),
     FOREIGN KEY (sec_id) REFERENCES securities(id) ON DELETE CASCADE
 ) PARTITION BY RANGE (date);
