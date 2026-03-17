5f9c65229a99b74099e63437d3f88370e970e5ed
Hsiaochieh Tsai <asky.hunsin@gmail.com>
2026-03-17 23:25:16 +0800

    Slightly update

--- scripts/setup_partitions.sh
+++ scripts/setup_partitions.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Creating default partitions for OHLCV tables..."
 
-docker exec -i compass-postgres psql -U compass -d compass <<EOF
+docker exec -i compass-postgres psql <<EOF
 CREATE TABLE IF NOT EXISTS ohlcv_per_min_default PARTITION OF ohlcv_per_min DEFAULT;
 CREATE TABLE IF NOT EXISTS ohlcv_per_30min_default PARTITION OF ohlcv_per_30min DEFAULT;
 CREATE TABLE IF NOT EXISTS ohlcv_per_day_default PARTITION OF ohlcv_per_day DEFAULT;
